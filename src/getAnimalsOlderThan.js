d4eef87e3dc2d0764f90e2eae8df874b3d21e8ea
Tarcisio Liboni <tarcisioliboni@gmail.com>
2022-01-25 16:20:51 -0300

    Request 02 OK

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+function getAnimalsOlderThan(animal, animalAge) {
+  const selectedAnimal = species.find(({ name }) => name === animal);
+  return selectedAnimal.residents.every(({ age }) => age >= animalAge);
 }
 
 module.exports = getAnimalsOlderThan;
