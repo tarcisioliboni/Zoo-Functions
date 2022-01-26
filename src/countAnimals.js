9bc21d5a6ba00155128f66b5afe7d41f65de73cd
Tarcisio Liboni <tarcisioliboni@gmail.com>
2022-01-26 14:54:40 -0300

    Request 05 OK

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,29 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const newObj = {};
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal == null) {
+    for (let index = 0; index < species.length; index += 1) {
+      newObj[species[index].name] = species[index].residents.length;
+    }
+    return newObj;
+  }
+  const aniSpecies = Object.values(animal)[0];
+  const aniSex = Object.values(animal)[1];
+
+  if (Object.values(animal).length === 1) {
+    return data.species.find((specie) =>
+      (specie.name === aniSpecies)).residents.length;
+  }
+
+  if (Object.values(animal).length === 2) {
+    return data.species.find((specie) =>
+      (specie.name === aniSpecies)).residents.filter((sexType) => sexType.sex === aniSex).length;
+  }
 }
 
 module.exports = countAnimals;
+
+console.log(countAnimals());
