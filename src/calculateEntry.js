50778c11dd0964b2ea2d525daf2ba555dab1be09
Tarcisio Liboni <tarcisioliboni@gmail.com>
2022-01-26 19:07:51 -0300

    Request 06 OK

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,49 +1,37 @@
 const { name } = require('faker/locale/pt_BR');
 const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
-newObj = {};
 
-let sumChild = 0;
-let sumAdult = 0;
-let sumSenior = 0;
+const counterPeople = (people) => {
+  const output = { child: 0, adult: 0, senior: 0};
+  people.forEach((person) => {
+    if (person.age < 18) output.child += 1;
+    else if (person.age >= 18 && person.age < 50) output.adult += 1;
+    else output.senior += 1;
+  });
+  return output;
+}
 
 function countEntrants(entrants) {
-  if (Object.keys(entrants).length === 0 || entrants == null) {
-    return 0;
-  }
-  for (let index = 0; index < entrants.length; index += 1) {
-    let entrantAge = entrants[index].age;
-    if (entrantAge < 18) {
-      sumChild += 1;
-    }
-    if (entrantAge >= 18 && entrantAge < 50) {
-      sumAdult += 1;
-    } else if (entrantAge >=50) {
-      sumSenior += 1;
-    }
-  }
-newObj['child'] = sumChild;
-newObj['adult'] = sumAdult;
-newObj['senior'] = sumSenior;
-  return newObj;
+  return counterPeople(entrants);
 }
 
 function calculateEntry(entrants) {
-  if (Object.keys(entrants).length === 0 || entrants == null) {
-    return 0;
-  }
-  let totalValue = ((sumChild * prices.child)
-  + (sumAdult * prices.adult)
-  + (sumSenior * prices.senior));
+  if (!entrants || entrants.length === undefined) return 0;
+  let totalValue = 0;
+  const guests = countEntrants(entrants)
+
+  totalValue = guests.child * prices.child
+  + guests.adult * prices.adult
+  + guests.senior * prices.senior;
+
   return totalValue;
 }
 
 module.exports = { calculateEntry, countEntrants };
 
-// const entrants = [
-//   { name: name.findName(), age: 50 }
-//   ];
+// const entrants = {};
+  
   
 // console.log(countEntrants(entrants));
 // console.log(calculateEntry(entrants));
-
